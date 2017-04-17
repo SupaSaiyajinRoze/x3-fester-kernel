6a9902b103078836265c717e170fc2cf42d91d1a
DarkAbhi <basevd94@gmail.com>
2017-04-17 15:53:36 +0530

    Custom Script

--- build.sh
+++ build.sh
@@ -1,216 +1,90 @@
 #!/bin/bash
-# Default settings
-verfile="android.ver"
-curcfg=".config"
-release="n"
-rebuild="n"
-clean="n"
-makeflags="-w"
-makedefs="V=0"
-makejobs=${MAKEJOBS}
-curdir=`pwd`
-if [ "${KBUILD_OUTPUT_SUPPORT}" == "yes" ];then
-  outdir=../${MTK_ROOT_OUT}/KERNEL_OBJ
-  mkdir -p $outdir
-  mkdir -p mediatek/custom
-  ln -fns ../../../${MTK_ROOT_CUSTOM_OUT} mediatek/custom/out
+#
+
+#Custom Build Script
+
+#
+# Copyright © 2016, "DarkAbhi" <basevd94@gmail.com>
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# Please maintain this if you use this script or any part of it
+#
+
+# Init Script
+KERNEL_DIR=$PWD
+KERNEL="Image.gz-dtb"
+KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
+BASE_VER="FESTER"
+VER="-v1-$(date +"%Y-%m-%d"-%H%M)-"
+BUILD_START=$(date +"%s")
+
+
+# Color Code Script
+black='\e[0;30m'        # Black
+red='\e[0;31m'          # Red
+green='\e[0;32m'        # Green
+yellow='\e[0;33m'       # Yellow
+blue='\e[0;34m'         # Blue
+purple='\e[0;35m'       # Purple
+cyan='\e[0;36m'         # Cyan
+white='\e[0;37m'        # White
+nocol='\033[0m'         # Default
+
+# Tweakable Stuff
+export ARCH=arm64
+export CROSS_COMPILE=/home/basevd/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export JOBS=16
+export SUBARCH=arm
+export KBUILD_BUILD_USER="DarkAbhi"
+export KBUILD_BUILD_HOST="FESTERSERVER"
+
+
+#COMPILATION SCRIPTS
+echo -e "${green}"
+echo "--------------------------------------------------------"
+echo "      Initializing build to compile Ver: $VER    "
+echo "--------------------------------------------------------"
+
+echo -e "$yellow***********************************************"
+echo "          Compiling FESTER!!      "
+echo -e "***********************************************$nocol"
+
+rm -f $KERN_IMG
+
+echo -e "$red***********************************************"
+echo "          Cleaning Up Before Compile          "
+echo -e "***********************************************$nocol"
+
+$clean rm -rf out && mkdir out
+
+echo -e "$yellow***********************************************"
+echo "          Initialising DEFCONFIG        "
+echo -e "***********************************************$nocol"
+
+make -C $PWD O=$PWD/out ARCH=arm64 x500_defconfig 
+
+echo -e "$yellow***********************************************"
+echo "          Cooking FESTER!!        "
+echo -e "***********************************************$nocol"
+
+make -j$JOBS -C $PWD O=$PWD/out ARCH=arm64 KCFLAGS=-mno-android 
+
+if ! [ -a $ZIMAGE ];
+then
+echo -e "$Red Kernel Compilation failed! Fix the errors! $nocol"
+exit 1
 fi
 
-usage() {
-    echo "Usage: $0 {release|rebuild|clean|silent|verbose|single} [config-xxx]"
-    echo "  config file will be generated if build with TARGET_PRODUCT"
-    exit 1
-}
 
-make_clean() {
-    echo "**** Cleaning ****"
-    nice make ${makeflags} ${makedefs} distclean
-}
-
-
-# Main starts here
-while test -n "$1"; do
-    case "$1" in
-    release)
-        release="y"
-    ;;
-    rebuild)
-        rebuild="y"
-    ;;
-    clean)
-        clean="y"
-    ;;
-    mrproper)
-        mrproper="y"
-    ;;
-    silent)
-        makeflags="-ws"
-        makedefs="V=0"
-    ;;
-    verbose)
-        makeflags="-w"
-        makedefs="V=1"
-    ;;
-    single)
-        makejobs=""
-    ;;
-    *)
-        export TARGET_PRODUCT=$1
-    ;;
-    esac
-    shift
-done
-
-source ../mediatek/build/shell.sh ../ kernel
-defcfg="${MTK_ROOT_GEN_CONFIG}/kconfig"
-if [ "${KBUILD_OUTPUT_SUPPORT}" == "yes" ]; then
-  makeflags+=" O=$outdir"
-fi
-
-if [ ! -z $KMOD_PATH ]; then
-  if [ ! -e $KMOD_PATH ]; then
-     echo "Invalid KMOD_PATH:$KMOD_PATH"
-     echo "CURDIR=$curdir"
-     exit 1;
-  fi
-fi
-
-# clean if it is necessary
-if [ "${clean}" == "y" ]; then
-   if [ ! -z $KMOD_PATH ]; then
-      echo "Clean kernel module PROJECT=$MTK_PROJECT PATH=$KMOD_PATH";
-      if [ "${KBUILD_OUTPUT_SUPPORT}" == "yes" ]; then
-        make M="$KMOD_PATH" O=$outdir clean
-      else
-        make M="$KMOD_PATH" clean
-      fi
-      exit $?
-   else
-      make_clean; exit $?;
-   fi
-fi
-
-if [ "${mrproper}" == "y" ]; then
-  make mrproper; exit $?;
-fi
-
-if [ "${rebuild}" == "y" ]; then make_clean; fi
-
-echo "**** Configuring / $defcfg / ****"
-# select correct configuration file
-if [ "${KBUILD_OUTPUT_SUPPORT}" == "yes" ]; then
-  make mediatek-configs O=$outdir
-else
-  make mediatek-configs
-fi
-
-# Config DRAM size according to central Project Configuration file setting
-# Todo:
-# Need a robust mechanism to control Kconfig content by central Config setting
-# Move below segment to a configuration file for extension
-if [ "$CUSTOM_DRAM_SIZE" == "3G" ]; then
-    # Config DRAM size as 3G (0x18000000).
-    sed --in-place=.orig \
-        -e 's/\(CONFIG_MAX_DRAM_SIZE_SUPPORT=\).*/\10x18000000/' \
-        $outdir/.config
-else
-  if [ "$CUSTOM_DRAM_SIZE" == "2G" ]; then
-      # Config DRAM size as 2G (0x10000000).
-      sed --in-place=.orig \
-          -e 's/\(CONFIG_MAX_DRAM_SIZE_SUPPORT=\).*/\10x10000000/' \
-          -e 's/\(CONFIG_RESERVED_MEM_SIZE_FOR_PMEM=\).*/\10x1700000/' \
-          $outdir/.config
-  else
-    if [ "$CUSTOM_DRAM_SIZE" == "4G" ]; then
-        # Config DRAM size as 4G (0x20000000).
-        sed --in-place=.orig \
-            -e 's/\(CONFIG_MAX_DRAM_SIZE_SUPPORT=\).*/\10x20000000/' \
-            $outdir/.config
-    else
-      if [ "$CUSTOM_DRAM_SIZE" == "6G" ]; then
-          # Config DRAM size as 6G (0x30000000).
-          sed --in-place=.orig \
-              -e 's/\(CONFIG_MAX_DRAM_SIZE_SUPPORT=\).*/\10x30000000/' \
-              -e 's/\(CONFIG_CMDLINE=.*\)"/\1 vmalloc=280M"/' \
-              -e 's/.*\(CONFIG_HIGHMEM\).*/\1=y/' \
-              -e '$ a\# CONFIG_HIGHPTE is not set' \
-              -e '$ a\# CONFIG_DEBUG_HIGHMEM is not set' \
-              $outdir/.config
-      else
-        if [ "$CUSTOM_DRAM_SIZE" == "8G" ]; then
-            # Config DRAM size as 8G (0x40000000).
-            sed --in-place=.orig \
-                -e 's/\(CONFIG_MAX_DRAM_SIZE_SUPPORT=\).*/\10x40000000/' \
-                -e 's/\(CONFIG_CMDLINE=.*\)"/\1 vmalloc=280M"/' \
-                -e 's/.*\(CONFIG_HIGHMEM\).*/\1=y/' \
-                -e '$ a\# CONFIG_HIGHPTE is not set' \
-                -e '$ a\# CONFIG_DEBUG_HIGHMEM is not set' \
-                $outdir/.config
-        fi
-      fi
-    fi
-  fi
-fi
-
-# update configuration
-nice make ${makeflags} ${makedefs} silentoldconfig
-
-if [ ! -z $KMOD_PATH ]; then
-  echo "Build kernel module PROJECT=$MTK_PROJECT PATH=$KMOD_PATH";
-  if [ "${KBUILD_OUTPUT_SUPPORT}" == "yes" ]; then
-    make M="$KMOD_PATH" O=$outdir modules
-  else
-    make M="$KMOD_PATH" modules
-  fi
-  exit $?
-fi
-
-echo "**** Building ****"
-make ${makeflags} ${makejobs} ${makedefs}
-
-if [ $? -ne 0 ]; then exit 1; fi
-
-echo "**** Successfully built kernel ****"
-
-mkimg="${MTK_ROOT_BUILD}/tools/mkimage"
-if [ "${KBUILD_OUTPUT_SUPPORT}" == "yes" ]; then
-  if [ "${TARGET_ARCH}" == "arm64" ]; then
-    kernel_img="${outdir}/arch/arm64/boot/Image"
-    if [ "${MTK_APPENDED_DTB_SUPPORT}" == "yes" ]; then
-      kernel_zimg="${outdir}/arch/arm64/boot/Image.gz-dtb"
-    else
-      kernel_zimg="${outdir}/arch/arm64/boot/Image.gz"
-  else
-    kernel_img="${outdir}/arch/arm/boot/Image"
-    if [ "${MTK_APPENDED_DTB_SUPPORT}" == "yes" ]; then
-      kernel_zimg="${outdir}/arch/arm/boot/zImage-dtb"
-    else
-      kernel_zimg="${outdir}/arch/arm/boot/zImage"
-  fi
-else
-  if [ "${TARGET_ARCH}" == "arm64" ]; then
-    kernel_img="${curdir}/arch/arm64/boot/Image"
-    if [ "${MTK_APPENDED_DTB_SUPPORT}" == "yes" ]; then
-      kernel_zimg="${curdir}/arch/arm64/boot/Image.gz-dtb"
-    else
-      kernel_zimg="${curdir}/arch/arm64/boot/Image.gz"
-  else
-    kernel_img="${curdir}/arch/arm/boot/Image"
-    if [ "${MTK_APPENDED_DTB_SUPPORT}" == "yes" ]; then
-      kernel_zimg="${curdir}/arch/arm/boot/zImage-dtb"
-    else
-      kernel_zimg="${curdir}/arch/arm/boot/zImage"
-  fi
-fi
-
-echo "**** Generate download images ****"
-
-if [ ! -x ${mkimg} ]; then chmod a+x ${mkimg}; fi
-
-if [ "${KBUILD_OUTPUT_SUPPORT}" == "yes" ]; then
-  ${mkimg} ${kernel_zimg} KERNEL > $outdir/kernel_${MTK_PROJECT}.bin
-else
-  ${mkimg} ${kernel_zimg} KERNEL > kernel_${MTK_PROJECT}.bin
-fi
-
-copy_to_legacy_download_flash_folder   $outdir/kernel_${MTK_PROJECT}.bin rootfs_${MTK_PROJECT}.bin
+#BUILD TIME
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$Yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
